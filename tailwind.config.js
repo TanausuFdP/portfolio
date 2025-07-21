10c3f9acb2c16d09e0abf679e7925f036e1db97b
tanausufdp <tanausufdp@icloud.com>
2025-07-21 20:42:21 +0100

    chore: basic config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,7 +16,22 @@ const config = {
     },
   },
   darkMode: "class",
-  plugins: [heroui()],
-}
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            background: "#F9F9F9",
+          },
+        },
+        dark: {
+          colors: {
+            background: "#000000",
+          },
+        },
+      },
+    }),
+  ],
+};
 
 module.exports = config;
