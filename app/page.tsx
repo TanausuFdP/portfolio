22659f4913e04979cc483deb2445513cd8b4ce0b
tanausufdp <tanausufdp@icloud.com>
2025-07-22 16:16:55 +0100

    feat: topbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,19 @@ import Entry from "./components/entry";
 import Now from "./components/now";
 import Services from "./components/services";
 import Resume from "./components/resume";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Welcome />
-      <Entry />
-      <Now />
-      <Services />
-      <Resume />
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
+      <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <Welcome />
+        <Entry />
+        <Now />
+        <Services />
+        <Resume />
+      </div>
     </div>
   );
 }
